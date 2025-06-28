0a72242d09f5886146e926a4956d7fc3679b9507
Lincolm Villanueva <lincolmvf@gmail.com>
2025-06-27 20:05:09 -0500

    CAmbio de js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function saludar() {
-  alert("¡Hola! Esta es una prueba.");
+function saludar(nombre) {
+  if (!nombre) {
+    console.warn("No se proporcionó un nombre");
+    return;
+  }
+
+  const mensaje = `¡Hola, ${nombre}! Bienvenido.`;
+  console.log(mensaje);
 }
+
+saludar("Lincolm");
